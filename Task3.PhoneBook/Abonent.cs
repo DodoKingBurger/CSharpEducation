f4d2123f766ad9f95ea13e8660ba421c8f20e42a
DodoKingBurger <157513217+DodoKingBurger@users.noreply.github.com>
2024-08-20 09:40:44 +0400

    Update Abonent.cs - переделал в структуру
    Убрал конструктор потому что он мешал переделать класс в конструктор

--- Task3.PhoneBook/Abonent.cs
+++ Task3.PhoneBook/Abonent.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,27 +6,36 @@ using System.Threading.Tasks;
 
 namespace Task3.PhoneBook
 {
-  /// <summary>
-  /// Abonent - Класс Абонент представляет собой будуйщий экземпляр контанкта в телефонной книге
-  /// </summary>
-  internal class Abonent
+	/// <summary>
+	/// Abonent - Класс Абонент представляет собой будуйщий экземпляр контанкта в телефонной книге
+	/// </summary> 
+	internal struct Abonent
   {
     private long number;
     private string name;
     public long Number
     {
-      get 
-      { if(number == 0|| number == null) 
-          throw new ArgumentException("НЕ найден номер");
-      else
-          return this.number; 
+      get
+      {
+        return number;
       }
       set
       {
         if (value != null)
         {
-          this.number = value;
+          //if(lengh(value) == 11 || lengh(value) == 6 || lengh(value) == 12)
+          //{
+            this.number = value;
+          //}
+          //else
+          //{
+          //  Console.WriteLine("Такого номер не существует");
+          //}
         }
+        else
+        {
+					this.number = -1;
+				}
       }
     }
     public string Name
@@ -37,14 +46,26 @@ namespace Task3.PhoneBook
       }
       set
       {
+        if (string.IsNullOrEmpty(value))
+        {
+          this.name = "Аноним";
+        }
+        else
+        {
           this.name = value;
+        }
+
       }
     }
-    public Abonent(long number,string name = "Аноним")
-    {
-      Name = name;
-      Number = number;
-    }
-
+    //private int lengh(long value)
+    //{
+    //  int i = 0;
+    //  do
+    //  {
+    //    value /= 10;
+    //    i++;
+    //  }while (value>0);
+    //  return i;
+    //}
   }
 }
