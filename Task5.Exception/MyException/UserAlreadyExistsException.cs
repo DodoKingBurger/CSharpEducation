32044d756595ba1b3a27ef32146cf2d74d335a27
Danil Vasilevykhd <sarkisyan33@mail.ru>
2024-09-13 14:52:31 +0400

    Добавил документацию к ошибкам.

--- Task5.Exception/MyException/UserAlreadyExistsException.cs
+++ Task5.Exception/MyException/UserAlreadyExistsException.cs
@@ -7,6 +7,10 @@ namespace Task5.Exception
 {
 	internal class UserAlreadyExistsException : System.Exception
 	{
+		/// <summary>
+		/// Это исключение выбрасывается, если пользователь уже существует в списке сотрудников.
+		/// </summary>
+		/// <param name="message">Сообщение об ошибке с объяснением причины исключения.</param>
 		public UserAlreadyExistsException(string message)
 			: base(message) { }
 	}
--- Task5.Exception/MyException/UserNotFoundException.cs
+++ Task5.Exception/MyException/UserNotFoundException.cs
@@ -8,6 +8,10 @@ namespace Task5.Exception.MyException
 {
 	internal class UserNotFoundException : System.Exception
 	{
+		/// <summary>
+		/// Это исключение выбрасывается, если пользователь не был найден.
+		/// </summary>
+		/// <param name="message">Сообщение об ошибке с объяснением причины исключения.</param>
 		public UserNotFoundException(string message)
 	: base(message) { }
 	}
